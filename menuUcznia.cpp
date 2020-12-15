6c3768c2f70f573353eba0c4d58efb79efac137f
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2020-12-15 08:19:59 +0100

    Added .gitignore for .exe(s)

--- menuUcznia.cpp
+++ menuUcznia.cpp
@@ -7,30 +7,37 @@ int main()
   int num;
   time_t czas;
   time(&czas);
+  
+  int oceny[] = {5,4};
 
-start:
-  cout << "Wybierz numer. \n 0 = zakonczenie programu. \n 1-2 = uczniowie o wybranym numerze \n Numer: ";
-  cin >> num;
-  switch (num)
-  {
-  case 0:
-    return 0;
-    break;
-  default:
-    // PODAWANIE LICZBY
-    if (num == 1 || num == 2)
+  start:
+    cout << "Wybierz numer.\n  0 = zakonczenie programu. \n  1-2 = uczniowie o wybranym numerze \nNumer: ";
+    cin >> num;
+    switch (num)
     {
-      cout << "Dobry numer" << endl;
+    case 0:
+      return 0;
+      break;
+    default:
+    cout << "---------------------------" << endl;
+    cout << "---------------------------" << endl;
+    cout << "-------SPRAWDZANIE---------" << endl;
+    cout << "---------------------------" << endl;
+    cout << "---------------------------" << endl;
+      // PODAWANIE LICZBY
+      if (num == 1 || num == 2)
+      {
+        cout << "Uczen o tym numerze dostal ostatnio ocene "<< oceny[num-1] << endl;
+      }
+      else
+     {
+        cout << "Nie ma ucznia o takim numerze" << endl;
+        goto start;
+      }
+      // PODAWANIE CZASU
+      cout << "Jest teraz " << ctime(&czas) << endl;
+      break;
     }
-    else
-    {
-      cout << "Nie ma ucznia o takim numerze" << endl;
-      goto start;
-    }
-    // PODAWANIE CZASU
-    cout << "Jest teraz " << ctime(&czas) << endl;
-    break;
-  }
   cout << "---------------" << endl;
   goto start;
 }
